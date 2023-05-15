de8471d37537284df82f5a62cdf3829414c2a1e1
Adam <adamcegielka@tlen.pl>
2023-05-15 14:57:01 +0200

    🔧 looking for a solution to a problem

--- AutomationExercise-Playwright/tests/testCase07.spec.ts
+++ AutomationExercise-Playwright/tests/testCase07.spec.ts
@@ -9,17 +9,19 @@ test.describe('Test Case 7: Verify Test Cases Page', () => {
     const urlCase = testCase07Data.urlCase;
 
     // 1. Launch browser
-    await chromium.launch();
+    await chromium.launch();    
     // 2. Navigate to url 'http://automationexercise.com'
     await page.goto(url);
     // 3. Verify that home page is visible successfully
     await expect(page).toHaveURL(url);
     // 4. Click on 'Test Cases' button
     await page.getByRole('button', { name: 'Test Cases' }).click();
+
+    // Error: https://automationexercise.com/#google_vignette:
+
+    await page.frameLocator('#aswift_5').locator('[aria-label="Close ad"]').click();
+    
     // 5. Verify user is navigated to test cases page successfully
-
-    // Error: https://automationexercise.com/#google_vignette
-
     await expect(page).toHaveURL(urlCase);
   });
 });
