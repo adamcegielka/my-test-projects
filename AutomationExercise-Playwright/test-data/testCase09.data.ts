5b3c4a63cf413563060776ee5699bb37d50ff34e
Adam <adamcegielka@tlen.pl>
2023-05-16 19:15:59 +0200

    🔧 Refactoring testCase09.data.ts

--- AutomationExercise-Playwright/test-data/testCase09.data.ts
+++ AutomationExercise-Playwright/test-data/testCase09.data.ts
@@ -1,3 +1,6 @@
 export const testCase09Data = {
-    url: 'https://automationexercise.com/'
+    url: 'https://automationexercise.com/',
+    messageProducts: 'ALL PRODUCTS',
+    productName: 'Men Tshirt',
+    allProductsSearch: 'Searched Products  Added! Your product has been added to cart. View Cart Contin'
 }
