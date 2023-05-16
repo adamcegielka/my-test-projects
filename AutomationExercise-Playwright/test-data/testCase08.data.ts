133fde0fd6f61fe80295b0f5c6eef24d4df47c3f
Adam <adamcegielka@tlen.pl>
2023-05-16 18:27:30 +0200

    🚀 addition new Test Case 8

---
+++ AutomationExercise-Playwright/test-data/testCase08.data.ts
@@ -0,0 +1,4 @@
+export const testCase08Data = {
+    url: 'https://automationexercise.com/',
+    urlProductDetails: 'https://automationexercise.com/product_details/1'
+}
---
+++ AutomationExercise-Playwright/tests/testCase08.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect, chromium } from '@playwright/test';
+import { testCase08Data } from '../test-data/testCase08.data';
+
+test.describe('Test Case 7: Verify Test Cases Page', () => {
+  // Verify Test Cases Page
+  test.fixme('verify test cases page', async ({ page }) => {
+    const url = testCase08Data.url;
+    const urlProductDetails = testCase08Data.urlProductDetails;
+
+    // 1. Launch browser
+    await chromium.launch();
+
+    // 2. Navigate to url 'http://automationexercise.com'
+    await page.goto(url);
+
+    // 3. Verify that home page is visible successfully
+    await expect(page).toHaveURL(url);
+
+    // 4. Click on 'Products' button
+    await page.getByRole('link', { name: ' Products' }).click();
+
+    // Exit from Google ads
+    // await page.frameLocator('iframe[id="ad_position_box"]').frameLocator('iframe[id="dismiss-button"]').getByRole('button', { name: 'Close ad' }).click();
+    // await page.frameLocator('iframe[name="aswift_5"]').frameLocator('iframe[name="ad_iframe"]').getByRole('button', { name: 'Close ad' }).click();
+
+    // 5. Verify user is navigated to ALL PRODUCTS page successfully
+    await expect(page.getByText('ALL PRODUCTS')).toBeVisible();
+
+    // 6. The products list is visible
+    await expect(page.getByText('All Products  Added! Your product has been added to cart. View Cart Continue Sh')).toBeVisible();
+
+    // 7. Click on 'View Product' of first product
+    await page.locator('.choose > .nav > li > a').first().click();
+
+    // 8. User is landed to product detail page
+    await expect(page).toHaveURL(urlProductDetails);
+    await expect(page.getByText('Blue Top Category: Women > Tops Rs. 500 Quantity: Add to cart Availability: In S')).toBeVisible();
+
+    // 9. Verify that detail detail is visible: product name, category, price, availability, condition, brand
+    await expect(page.getByRole('heading', { name: 'Blue Top' })).toBeVisible();
+    await expect(page.getByText('Category: Women > Tops')).toBeVisible();
+    await expect(page.getByText('Rs. 500')).toBeVisible();
+    await expect(page.getByText('Availability: In Stock')).toBeVisible();
+    await expect(page.getByText('Condition: New')).toBeVisible();
+    await expect(page.getByText('Brand: Polo')).toBeVisible();
+  });
+});
