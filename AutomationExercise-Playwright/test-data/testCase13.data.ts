1c7e385e8af9a0f7d2941ecb238dde71fb752e04
Adam <adamcegielka@tlen.pl>
2023-05-21 11:21:08 +0200

    🔧 Refactoring testCase13.data.ts

--- AutomationExercise-Playwright/test-data/testCase13.data.ts
+++ AutomationExercise-Playwright/test-data/testCase13.data.ts
@@ -1,3 +1,6 @@
 export const testCase13Data = {
-    url: 'https://automationexercise.com/'
+    url: 'https://automationexercise.com/',
+    verifyDetail: 'Stylish Dress Category: Women > Dress Rs. 1500 Quantity: Add to cart Availabilit',
+    verifyProductDisplayed: 'Product Image Stylish Dress Women > Dress Rs. 1500 4 Rs. 6000 ',
+    quantityProduct: '4'
 }
