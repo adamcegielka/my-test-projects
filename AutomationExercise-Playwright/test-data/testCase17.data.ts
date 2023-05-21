8d2d7b9e19d70a8768997ad937dac213cf1dfde8
Adam <adamcegielka@tlen.pl>
2023-05-21 17:02:55 +0200

    🚀 addition new testCase17.data.ts

---
+++ AutomationExercise-Playwright/test-data/testCase17.data.ts
@@ -0,0 +1,6 @@
+export const testCase17Data = {
+    url: 'https://automationexercise.com/',
+    urlCart: 'https://automationexercise.com/view_cart',    
+    verifyShoppingCart: 'Shopping Cart',
+    verifyProductRemoved: 'Cart is empty!'
+}
