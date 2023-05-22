2b7c76d824be0eb8c7928434769f96f3dbd1c6f7
Adam <adamcegielka@tlen.pl>
2023-05-22 12:13:45 +0200

    🔧 code refactoring

--- AutomationExercise-Playwright/test-data/testCase20.data.ts
+++ AutomationExercise-Playwright/test-data/testCase20.data.ts
@@ -1,3 +1,10 @@
 export const testCase20Data = {
-    url: 'https://automationexercise.com/'
+    url: 'https://automationexercise.com/',
+    email: 'adrian.jackson@mail.com',
+    userPassword: '1a2b3c4d',
+    verifyProducts: 'All Products',
+    verifyProductsAll: 'All Products  Added! Your product has been added to cart. View Cart Continue Sh',
+    verifyProductsSearched: 'Searched Products',
+    verifyProductsSearchedAll: 'Searched Products  Added! Your product has been added to cart. View Cart Contin',
+    verifyProductsInCart: 'Product Image Blue Top Women > Tops Rs. 500 1 Rs. 500 '
 }
