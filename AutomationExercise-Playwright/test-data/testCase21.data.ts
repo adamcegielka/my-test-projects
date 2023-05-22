23ee289a38bd3bb4a105cf615ae28a02b391d2e7
Adam <adamcegielka@tlen.pl>
2023-05-22 12:38:33 +0200

    🚀 addition new testCase21.data.ts

---
+++ AutomationExercise-Playwright/test-data/testCase21.data.ts
@@ -0,0 +1,10 @@
+export const testCase21Data = {
+    url: 'https://automationexercise.com/',
+    email: 'adrian.jackson@mail.com',
+    userPassword: '1a2b3c4d',    
+    nameSurname: 'Adrian Jackson',
+    review: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    verifyProducts: 'All Products',
+    verifyProductsAll: 'All Products  Added! Your product has been added to cart. View Cart Continue Sh',
+    verifySuccessMessage: 'Thank you for your review.'
+}
