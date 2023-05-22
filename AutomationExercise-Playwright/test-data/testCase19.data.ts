2b8a5f736ba66cea1c9dfccf579a60fa6eec3bfb
Adam <adamcegielka@tlen.pl>
2023-05-22 09:05:19 +0200

    🚀 addition new testCase19.data.ts

---
+++ AutomationExercise-Playwright/test-data/testCase19.data.ts
@@ -0,0 +1,8 @@
+export const testCase19Data = {
+    url: 'https://automationexercise.com/',
+    verifyBrandsPolo: 'Brands (6)Polo (5)H&M (5)Madame (3)Mast & Harbour (4)Babyhug (3)Allen Solly Juni',
+    verifyBrandPoloProducts: 'Brand - Polo Products',
+    verifyBrandPoloProductsAll: 'Brand - Polo Products  Added! Your product has been added to cart. View Cart Co',
+    verifyBrandBabyhugProducts: 'Brand - Babyhug Products',
+    verifyBrandBabyhugProductsAll: 'Brand - Babyhug Products  Added! Your product has been added to cart. View Cart'
+}
