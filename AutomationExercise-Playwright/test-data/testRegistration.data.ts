26b2ddaa1263641dd0389bd6203dc598c3ad9d10
Adam <adamcegielka@tlen.pl>
2023-05-22 09:30:31 +0200

    🚀 addition new testRegistration.data.ts

---
+++ AutomationExercise-Playwright/test-data/testRegistration.data.ts
@@ -0,0 +1,18 @@
+export const testRegistration = {
+    url: 'https://automationexercise.com/',
+    userId: 'AdrianoJ',
+    email: 'adrian.jackson@mail.com',
+    userPassword: '1a2b3c4d',
+    firstName: 'Adrian',
+    lastName: 'Jackson',
+    birthDay: '9',
+    birthMonth: '5',
+    birthYear: '1985',
+    companyName: 'Porta Limited',
+    address1: '50 Herzi Street APT 93',
+    zipCode: '84739',
+    city: 'Washington',
+    state: 'Washington',
+    country: 'United States',
+    mobileNumber: '801222333'
+}
