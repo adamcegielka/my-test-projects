2e6f34965a1bf0011cd228a38c414d9d3c46f0ed
Adam <adamcegielka@tlen.pl>
2023-05-22 16:23:41 +0200

    🔧 data update

--- AutomationExercise-Playwright/test-data/testCase24.data.ts
+++ AutomationExercise-Playwright/test-data/testCase24.data.ts
@@ -16,16 +16,18 @@ export const testCase24Data = {
     state: 'Washington',
     country: 'United States',
     mobileNumber: '801222333',
-    cardVendor: 'Visa',
+    cardName: 'Adrian Jackson',
     cardNumber: '4070723055941987',
     cardCvv: '981',
     cardExpirationateMonth: '11',
     cardExpirationateYear: '25',    
-    messageOrderMessage: 'Your order has been placed successfully!',
+    messageOrderSuccessfully: 'Your order has been placed successfully!',
+    messageText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     verifyNameSurname: 'Mr. Adrian Jackson',
     verifyCountryCityZip: 'Washington Washington 84739',
     verifyShoppingCart: 'Shopping Cart',
     verifyAccountCreated: 'ACCOUNT CREATED!',
     verifyAccountDeleted: 'ACCOUNT DELETED!',
-    verifyDeliveryAddress: 'Your delivery address Mr. Adrian Jackson Porta Limited 50 Herzi Street APT 93 Wa'
+    verifyDeliveryAddress: 'Your delivery address Mr. Adrian Jackson Porta Limited 50 Herzi Street APT 93 Wa',
+    verifyReviewOrder: 'Product Image Winter Top Women > Tops Rs. 600 1 Rs. 600'
 }
