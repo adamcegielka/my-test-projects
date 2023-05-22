87cc57c52584349b51d462ad793d2a193a463e31
Adam <adamcegielka@tlen.pl>
2023-05-22 13:18:27 +0200

    🚀 addition new testCase22.data.ts

---
+++ AutomationExercise-Playwright/test-data/testCase22.data.ts
@@ -0,0 +1,5 @@
+export const testCase22Data = {
+    url: 'https://automationexercise.com/',
+    verifyRecommendedItems: 'RECOMMENDED ITEMS',
+    verifyProductIsDisplayes: 'Product Image Men Tshirt Men > Tshirts Rs. 400 1 Rs. 400 '
+}
