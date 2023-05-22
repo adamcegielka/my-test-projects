df7f999935c58f8574a034d7ea322d3a9be9c428
Adam <adamcegielka@tlen.pl>
2023-05-22 14:01:00 +0200

    🚀 addition new testCase23.data.ts

---
+++ AutomationExercise-Playwright/test-data/testCase23.data.ts
@@ -0,0 +1,6 @@
+export const testCase23Data = {
+    url: 'https://automationexercise.com/',
+    email: 'adrian.jackson@mail.com',
+    verifyTextSubscription: 'SUBSCRIPTION',
+    verifySuccessMessage: 'You have been successfully subscribed!'
+}
