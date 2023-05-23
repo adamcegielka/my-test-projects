a3c14019f16558d605b2f0c8ea2f2a25f853dd5c
Adam <adamcegielka@tlen.pl>
2023-05-23 13:22:15 +0200

    🔧 code refactoring

--- AutomationExercise-Playwright/test-data/testCase02.data.ts
+++ AutomationExercise-Playwright/test-data/testCase02.data.ts
@@ -1,18 +1,5 @@
 export const testCase02Data = {
-    url: 'https://automationexercise.com/',
-    userId: 'Jan Nowak',
-    userEmail: 'nowaki@tester.com',
-    userPassword: 'nowy234',
-    birthDay: '11',
-    birthMonth: '5',
-    birthYear: '1996',
-    firstName: 'Jan',
-    lastName: 'Nowak',
-    companyName: 'Nowinki',
-    address1: 'Nowa 4',
-    country: 'United States',
-    state: 'Dover',
-    city: 'Delaware',
-    zipcode: '19906',
-    mobileNumber: '8223334444'
+    verifyHomePage: 'Automation Exercise',
+    verifyLoginInAccount: 'New User Signup!',
+    verifAccountDeleted: 'ACCOUNT DELETED!'
 }
--- AutomationExercise-Playwright/tests/testCase02.spec.ts
+++ AutomationExercise-Playwright/tests/testCase02.spec.ts
@@ -1,80 +1,60 @@
 import { test, expect, chromium } from '@playwright/test';
 import { testCase02Data } from '../test-data/testCase02.data';
+import { testRegistrationData } from '../test-data/testRegistration.data';
+import { registerUser } from './testRegistration.spec';
+import { LoginPage } from '../pages/login.page';
 
 test.describe('Test Case 2: login User with correct email and password', () => {
 
-  // Creation of a new user before the test
-  test('creation of a new user before the test', async ({ page }) => {
-    const url = testCase02Data.url;
-    const userId = testCase02Data.userId;
-    const userEmail = testCase02Data.userEmail;
-    const userPassword = testCase02Data.userPassword;
-    const birthDay = testCase02Data.birthDay;
-    const birthMonth = testCase02Data.birthMonth;
-    const birthYear = testCase02Data.birthYear;
-    const firstName = testCase02Data.firstName;
-    const lastName = testCase02Data.lastName;
-    const companyName = testCase02Data.companyName;
-    const address1 = testCase02Data.address1; 
-    const country = testCase02Data.country;
-    const state = testCase02Data.state;
-    const city = testCase02Data.city;
-    const zipcode = testCase02Data.zipcode;
-    const mobileNumber = testCase02Data.mobileNumber;
-    
-    await page.goto(url);   
-    await page.getByRole('link', { name: ' Signup / Login' }).click(); 
-    await page.getByPlaceholder('Name').fill(userId); 
-    await page.locator('form').filter({ hasText: 'Signup' }).getByPlaceholder('Email Address').fill(userEmail); 
-    await page.getByRole('button', { name: 'Signup' }).click();  
-    await page.getByLabel('Mr.').check();
-    await page.getByLabel('Password *').fill(userPassword);
-    await page.locator('#days').selectOption(birthDay);
-    await page.locator('#months').selectOption(birthMonth);
-    await page.locator('#years').selectOption(birthYear);
-    await page.getByLabel('Sign up for our newsletter!').check();
-    await page.getByLabel('Receive special offers from our partners!').check();
-    await page.getByLabel('First name *').fill(firstName);
-    await page.getByLabel('Last name *').fill(lastName);
-    await page.getByLabel('Company', { exact: true }).fill(companyName);
-    await page.getByLabel('Address * (Street address, P.O. Box, Company name, etc.)').fill(address1);
-    await page.getByRole('combobox', { name: 'Country *' }).selectOption(country);
-    await page.getByLabel('State *').fill(state);
-    await page.getByLabel('City *').fill(city);
-    await page.locator('#zipcode').fill(zipcode);
-    await page.getByLabel('Mobile Number *').fill(mobileNumber);
-    await page.getByRole('button', { name: 'Create Account' }).click();
-    await page.getByRole('link', { name: 'Continue' }).click();
-    await page.getByRole('link', { name: ' Logout' }).click();    
-  });
+  // Creation a new user before the test
+  test.beforeEach(registerUser);
 
-  // Test Case 2: login User with correct email and password'
-  test('login User with correct email and password', async ({ page }) => {
-    const url = testCase02Data.url;
-    const userEmail = testCase02Data.userEmail;
-    const userPassword = testCase02Data.userPassword;
+  // Test Case 2: login User with correct email and password
+  test('login user with correct email and password', async ({ page }) => {
+    const userId = testRegistrationData.userId;
+    const userEmail = testRegistrationData.userEmail;
+    const userPassword = testRegistrationData.userPassword;
+
+    const verifyHomePage = testCase02Data.verifyHomePage;
+    const verifyLoginInAccount = testCase02Data.verifyLoginInAccount;
+    const verifAccountDeleted = testCase02Data.verifAccountDeleted;
 
     // 1. Launch browser    
     await chromium.launch();
+
     // 2. Navigate to url 'http://automationexercise.com'
-    await page.goto(url); 
+    await page.goto('/');
+
     // 3. Verify that home page is visible successfully
-    await expect(page).toHaveURL(url);   
+    await expect(page).toHaveURL('/');
+    await expect(page).toHaveTitle(verifyHomePage);
+
     // 4. Click on 'Signup / Login' button
-    await page.getByRole('link', { name: ' Signup / Login' }).click(); 
+    await page.getByRole('link', { name: 'Signup / Login' }).click();
+
     // 5. Verify 'Login to your account' is visible
-    await expect(page.getByText('New User Signup!')).toBeVisible(); 
+    await expect(page.getByText(verifyLoginInAccount)).toBeVisible();
+
     // 6. Enter correct email address and password
-    await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill(userEmail);
-    await page.getByPlaceholder('Password').fill(userPassword);
+    // POM - Page Object Model
+    const loginPage = new LoginPage(page);
+    await loginPage.userEmail.fill(userEmail);
+    await loginPage.userPassword.fill(userPassword);
+
     // 7. Click 'login' button
-    await page.getByRole('button', { name: 'Login' }).click();
+    await loginPage.loginButton.click();
+
     // 8. Verify that 'Logged in as username' is visible
-    await expect(page.getByText('Logged in as Jan Nowak')).toBeVisible();
+    await expect(page.getByText(`Logged in as ${userId}`)).toBeVisible();
+
     // 9. Click 'Delete Account' button
-    await page.getByRole('link', { name: ' Delete Account' }).click();
+    await page.getByRole('link', { name: 'Delete Account' }).click();
+
     // 10. Verify that 'ACCOUNT DELETED!' is visible
-    await expect(page.getByText('ACCOUNT DELETED!')).toBeVisible();
-    await page.getByRole('link', { name: 'Continue' }).click(); 
+    await expect(page.getByText(verifAccountDeleted)).toBeVisible();
+    await page.getByRole('link', { name: 'Continue' }).click();
+
+    // CLOSE BROWSER
+    await page.close();
   });  
 });
