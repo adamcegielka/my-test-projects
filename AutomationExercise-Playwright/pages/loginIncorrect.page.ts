f16347f0461acd01e206fc535d36249e74f3f662
Adam <adamcegielka@tlen.pl>
2023-05-23 13:59:51 +0200

    🚀 addition LoginIncorrectPage

---
+++ AutomationExercise-Playwright/pages/loginIncorrect.page.ts
@@ -0,0 +1,9 @@
+import { Page } from '@playwright/test';
+
+export class LoginIncorrectPage {
+  constructor(private page: Page) {}
+
+  userEmailIncorrect = this.page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address');
+  userPasswordIncorrect = this.page.getByPlaceholder('Password');
+  loginButton = this.page.getByRole('button', { name: 'Login' });
+}
