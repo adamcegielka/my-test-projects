b6f1be4de1f4937ddaf706713e784ae0c7be1806
Adam <adamcegielka@tlen.pl>
2023-05-23 09:40:12 +0200

    🔧 code refactoring

--- AutomationExercise-Playwright/tests/testUserDeletion.spec.ts
+++ AutomationExercise-Playwright/tests/testUserDeletion.spec.ts
@@ -1,16 +1,17 @@
 import { testUserDeletion } from '../test-data/testUserDeletion.data';
 
 export async function userDeletion({ page }) {
-  const url = testUserDeletion.url;  
   const email = testUserDeletion.email;
   const userPassword = testUserDeletion.userPassword;
 
-  await page.goto(url);
-  await page.getByRole('link', { name: ' Signup / Login' }).click();
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Signup / Login' }).click();
   await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill(email);
   await page.getByPlaceholder('Password').fill(userPassword);
   await page.getByRole('button', { name: 'Login' }).click();
 
-  await page.getByRole('link', { name: ' Delete Account' }).click();
+  await page.getByRole('link', { name: 'Delete Account' }).click();
   await page.getByRole('link', { name: 'Continue' }).click();
+
+  await page.close();
 };
