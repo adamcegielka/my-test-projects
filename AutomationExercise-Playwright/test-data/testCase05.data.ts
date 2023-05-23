808e5802d34cf55963400255a85455812c02bca3
Adam <adamcegielka@tlen.pl>
2023-05-23 15:29:22 +0200

    🔧 code refactoring

--- AutomationExercise-Playwright/test-data/testCase05.data.ts
+++ AutomationExercise-Playwright/test-data/testCase05.data.ts
@@ -1,19 +1,5 @@
 export const testCase05Data = {
-    url: 'https://automationexercise.com/',
-    userId: 'Olek Mucha',
-    userIdNew: 'Stive Mucha',
-    userEmail: 'mucha@tester.com',
-    userPassword: 'mucha974',
-    birthDay: '26',
-    birthMonth: '11',
-    birthYear: '1988',
-    firstName: 'Olek',
-    lastName: 'Mucha',
-    companyName: 'Meadow',
-    address1: 'Black 9',
-    country: 'United States',
-    state: 'Dover',
-    city: 'Delaware',
-    zipcode: '12507',
-    mobileNumber: '81113331111'
+    verifyHomePage: 'Automation Exercise',
+    verifyNewUserSignup: 'New User Signup!',
+    verifyEmailAlreadyExist: 'Email Address already exist!'
 }
--- AutomationExercise-Playwright/tests/testCase05.spec.ts
+++ AutomationExercise-Playwright/tests/testCase05.spec.ts
@@ -1,75 +1,53 @@
 import { test, expect, chromium } from '@playwright/test';
 import { testCase05Data } from '../test-data/testCase05.data';
+import { testRegistrationData } from '../test-data/testRegistration.data';
+import { registerUser } from './testRegistration.spec';
+import { userDeletion } from './testUserDeletion.spec';
+import { RegistrationPage } from '../pages/registration.page';
 
 test.describe('Test Case 5: Register User with existing email', () => {
 
-  // Creation of new user before the test
-  test('creation of a new user before the test', async ({ page }) => {
-    const url = testCase05Data.url;
-    const userId = testCase05Data.userId;
-    const userEmail = testCase05Data.userEmail;
-    const userPassword = testCase05Data.userPassword;
-    const birthDay = testCase05Data.birthDay;
-    const birthMonth = testCase05Data.birthMonth;
-    const birthYear = testCase05Data.birthYear;
-    const firstName = testCase05Data.firstName;
-    const lastName = testCase05Data.lastName;
-    const companyName = testCase05Data.companyName;
-    const address1 = testCase05Data.address1; 
-    const country = testCase05Data.country;
-    const state = testCase05Data.state;
-    const city = testCase05Data.city;
-    const zipcode = testCase05Data.zipcode;
-    const mobileNumber = testCase05Data.mobileNumber;
-    
-    await page.goto(url);   
-    await page.getByRole('link', { name: ' Signup / Login' }).click(); 
-    await page.getByPlaceholder('Name').fill(userId); 
-    await page.locator('form').filter({ hasText: 'Signup' }).getByPlaceholder('Email Address').fill(userEmail); 
-    await page.getByRole('button', { name: 'Signup' }).click();  
-    await page.getByLabel('Mr.').check();
-    await page.getByLabel('Password *').fill(userPassword);
-    await page.locator('#days').selectOption(birthDay);
-    await page.locator('#months').selectOption(birthMonth);
-    await page.locator('#years').selectOption(birthYear);
-    await page.getByLabel('Sign up for our newsletter!').check();
-    await page.getByLabel('Receive special offers from our partners!').check();
-    await page.getByLabel('First name *').fill(firstName);
-    await page.getByLabel('Last name *').fill(lastName);
-    await page.getByLabel('Company', { exact: true }).fill(companyName);
-    await page.getByLabel('Address * (Street address, P.O. Box, Company name, etc.)').fill(address1);
-    await page.getByRole('combobox', { name: 'Country *' }).selectOption(country);
-    await page.getByLabel('State *').fill(state);
-    await page.getByLabel('City *').fill(city);
-    await page.locator('#zipcode').fill(zipcode);
-    await page.getByLabel('Mobile Number *').fill(mobileNumber);
-    await page.getByRole('button', { name: 'Create Account' }).click();
-    await page.getByRole('link', { name: 'Continue' }).click();
-    await page.getByRole('link', { name: ' Logout' }).click();    
-  });
+  // Creation a new user before the test
+  test.beforeEach(registerUser);
+
+  // Deletion of a user after a test
+  test.afterEach(userDeletion);
 
   // Test Case 5: Register User with existing email
-  test('register User with existing email', async ({ page }) => {
-    const url = testCase05Data.url;
-    const userIdNew = testCase05Data.userIdNew;
-    const userEmail = testCase05Data.userEmail;
+  test('register user with existing email', async ({ page }) => {
+    const userIdNew = testRegistrationData.userIdNew;
+    const userEmail = testRegistrationData.userEmail;
+
+    const verifyHomePage = testCase05Data.verifyHomePage;
+    const verifyNewUserSignup = testCase05Data.verifyNewUserSignup;
+    const verifyEmailAlreadyExist = testCase05Data.verifyEmailAlreadyExist;
 
     // 1. Launch browser    
     await chromium.launch();
+
     // 2. Navigate to url 'http://automationexercise.com'
-    await page.goto(url); 
+    await page.goto('/');
+
     // 3. Verify that home page is visible successfully
-    await expect(page).toHaveURL(url);   
+    await expect(page).toHaveURL('/');
+    await expect(page).toHaveTitle(verifyHomePage);
+
     // 4. Click on 'Signup / Login' button
-    await page.getByRole('link', { name: ' Signup / Login' }).click(); 
+    await page.getByRole('link', { name: 'Signup / Login' }).click();
+
     // 5. Verify 'New User Signup!' is visible
-    await expect(page.getByText('New User Signup!')).toBeVisible();
+    await expect(page.getByText(verifyNewUserSignup)).toBeVisible();
+
     // 6. Enter name and already registered email address
-    await page.getByPlaceholder('Name').fill(userIdNew); 
-    await page.locator('form').filter({ hasText: 'Signup' }).getByPlaceholder('Email Address').fill(userEmail); 
+    // POM - Page Object Model
+    const registrationPage = new RegistrationPage(page);
+    await registrationPage.userIdNew.fill(userIdNew);
+    await registrationPage.userEmail.fill(userEmail);
+
     // 7. Click 'Signup' button
-    await page.getByRole('button', { name: 'Signup' }).click(); 
+    await registrationPage.signupButton.click();
+
     // 8. Verify error 'Email Address already exist!' is visible
-    await expect(page.getByText('Email Address already exist!')).toBeVisible();
+    await expect(page.getByText(verifyEmailAlreadyExist)).toBeVisible();
   });  
 });
