f4ddaa943f5828ef1cd353dc1497956a2c47da6f
Adam <adamcegielka@tlen.pl>
2023-05-23 17:06:12 +0200

    🔧 code refactoring

--- AutomationExercise-Playwright/tests/testCase06.spec.ts
+++ AutomationExercise-Playwright/tests/testCase06.spec.ts
@@ -1,42 +1,65 @@
 import { test, expect, chromium, } from '@playwright/test';
 import { testCase06Data } from '../test-data/testCase06.data';
+import { testRegistrationData } from '../test-data/testRegistration.data';
 
 test.describe('Test Case 6: Contact Us Form', () => {
+  
+  test('contact us form', async ({ page }) => {    
+    const userId = testRegistrationData.userId;
+    const userEmail = testRegistrationData.userEmail;
 
-  // Contact Us Form
-  test('contact Us Form', async ({ page }) => {
-    const url = testCase06Data.url;
-    const userId = testCase06Data.userId;
-    const userEmail = testCase06Data.userEmail;
     const subject = testCase06Data.subject;
     const message = testCase06Data.message;
 
+    const verifyHomePage = testCase06Data.verifyHomePage;
+    const verifyGetInTouch = testCase06Data.verifyGetInTouch;
+    const verifySuccessMessage = testCase06Data.verifySuccessMessage;
+
     // 1. Launch browser    
     await chromium.launch();
+
     // 2. Navigate to url 'http://automationexercise.com'
-    await page.goto(url); 
+    await page.goto('/');
+
     // 3. Verify that home page is visible successfully
-    await expect(page).toHaveURL(url);   
+    await expect(page).toHaveURL('/');
+    await expect(page).toHaveTitle(verifyHomePage);
+
     // 4. Click on 'Contact Us' button
-    await page.getByRole('link', { name: ' Contact us' }).click();
+    await page.getByRole('link', { name: 'Contact us' }).click();
+
     // 5. Verify 'GET IN TOUCH' is visible
-    await expect(page.getByText('GET IN TOUCH')).toBeVisible();
+    await expect(page.getByText(verifyGetInTouch)).toBeVisible();
+
     // 6. Enter name, email, subject and message
     await page.getByPlaceholder('Name').fill(userId);
     await page.getByPlaceholder('Email', { exact: true }).fill(userEmail);
     await page.getByPlaceholder('Subject').fill(subject);
     await page.getByPlaceholder('Your Message Here').fill(message);
+
     // 7. Upload file
-    await page.locator('input[name="upload_file"]').setInputFiles('myfile.txt');
+    await page.locator('input[name="upload_file"]').setInputFiles('upload-file/myfile.txt');
+
     // 8. Click 'Submit' button
     await page.getByRole('button', { name: 'Submit' }).click();
+
     // 9. Click OK button
     page.on('dialog', dialog => dialog.accept());
     await page.getByRole('button', { name: 'Submit' }).click();
+
     // 10. Verify success message 'Success! Your details have been submitted successfully.' is visible
-    await expect(page.locator('.status.alert.alert-success').getByText('Success! Your details have been submitted successfully.')).toBeVisible({ timeout: 5000 });
+    await expect(page.locator('.status.alert.alert-success').getByText(verifySuccessMessage)).toBeVisible({ timeout: 5000 });
+
     // 11. Click 'Home' button and verify that landed to home page successfully
-    await page.getByRole('link', { name: ' Home' }).click();
-    await expect(page).toHaveURL(url);
+    await page.locator('.fa.fa-angle-double-left').click();
+    
+    // EXIT FROM GOOGLE ADS
+    // await page.frameLocator('iframe[name="aswift_2"]').frameLocator('iframe[name="ad_iframe"]').getByRole('button', { name: 'Close ad' }).click();
+    
+    await expect(page).toHaveURL('/');
+    await expect(page).toHaveTitle(verifyHomePage);
+
+    // CLOSE BROWSER
+    await page.close();
   });  
 });
