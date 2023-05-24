e9d106a2d53098323bb93e987e6deb7c2315ff28
Adam <adamcegielka@tlen.pl>
2023-05-24 18:37:50 +0200

    🔧 code refactoring

--- AutomationExercise-Playwright/test-data/testCase10.data.ts
+++ AutomationExercise-Playwright/test-data/testCase10.data.ts
@@ -1,5 +1,4 @@
 export const testCase10Data = {
-    url: 'https://automationexercise.com/',
     subscription: 'Subscription',
     emailSubscription: 'example@email.com',
     messageSubscription: 'You have been successfully subscribed!'
--- AutomationExercise-Playwright/tests/testCase10.spec.ts
+++ AutomationExercise-Playwright/tests/testCase10.spec.ts
@@ -2,9 +2,8 @@ import { test, expect, chromium } from '@playwright/test';
 import { testCase10Data } from '../test-data/testCase10.data';
 
 test.describe('Test Case 10: Verify Subscription in home page', () => {
-  // Search Product
+  
   test('verify subscription in home page', async ({ page }) => {
-    const url = testCase10Data.url;
     const subscription = testCase10Data.subscription;
     const emailSubscription = testCase10Data.emailSubscription;
     const messageSubscription = testCase10Data.messageSubscription;
@@ -13,10 +12,10 @@ test.describe('Test Case 10: Verify Subscription in home page', () => {
     await chromium.launch();
 
     // 2. Navigate to url 'http://automationexercise.com'
-    await page.goto(url);
+    await page.goto('/');
 
     // 3. Verify that home page is visible successfully
-    await expect(page).toHaveURL(url);
+    await expect(page).toHaveURL('/');
 
     // 4. Scroll down to footer
     await page.evaluate(() => {
@@ -27,12 +26,19 @@ test.describe('Test Case 10: Verify Subscription in home page', () => {
 
     // 5. Verify text 'SUBSCRIPTION'
     await page.getByText(subscription).click();
+      // or:
+    const verifyText = await page.locator('.single-widget');
+    await expect(verifyText).toContainText(subscription);
 
     // 6. Enter email address in input and click arrow button
-    await page.getByPlaceholder('Your email address').fill(emailSubscription);
-    await page.getByRole('button', { name: '' }).click();
+      // await page.getByPlaceholder('Your email address').fill(emailSubscription);
+      // await page.getByRole('button', { name: '' }).click();
+      // or:
+    await page.type('#susbscribe_email', emailSubscription);
+    await page.click('#subscribe');
 
     // 7. Verify success message 'You have been successfully subscribed!' is visible
-    await page.getByText(messageSubscription).click();
+    const verifyMessage = await page.locator('#footer');
+    await expect(verifyMessage).toContainText(messageSubscription);
   });
 });
