c2c7fb0eedfbfffbdbd3aea0b4b2e45d6ff7442a
Adam <adamcegielka@tlen.pl>
2023-05-25 11:55:33 +0200

    🔧 code refactoring

--- AutomationExercise-Playwright/test-data/testCase14.data.ts
+++ AutomationExercise-Playwright/test-data/testCase14.data.ts
@@ -1,11 +1,6 @@
 export const testCase14Data = {
     verifyHomePage: 'Automation Exercise',
     urlCart: 'https://automationexercise.com/view_cart',
-    cardVendor: 'Visa',
-    cardNumber: '4070723055941987',
-    cardCvv: '981',
-    cardExpirationateMonth: '11',
-    cardExpirationateYear: '25',
     verifyAddress: 'Your delivery address Mr. Adrian Jackson Porta Limited 50 Herzi Street APT 9',
     verifyOrder: 'Product Image Sleeveless Dress Women > Dress Rs. 1000 1 Rs. 1000',
     messageText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
