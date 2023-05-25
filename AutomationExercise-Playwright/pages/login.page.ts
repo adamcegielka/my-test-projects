36551cb5c0f88dba579877ed508cadd0e6d8c70e
Adam <adamcegielka@tlen.pl>
2023-05-25 15:28:02 +0200

    🔧 update

--- AutomationExercise-Playwright/pages/login.page.ts
+++ AutomationExercise-Playwright/pages/login.page.ts
@@ -3,6 +3,7 @@ import { Page } from '@playwright/test';
 export class LoginPage {
   constructor(private page: Page) {}
 
+  signupLogin = this.page.getByRole('link', { name: 'Signup / Login' });
   userEmail = this.page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address');
   userPassword = this.page.getByPlaceholder('Password');
   loginButton = this.page.getByRole('button', { name: 'Login' });
