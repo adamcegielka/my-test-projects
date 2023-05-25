4aee22a1ab8c92c6558e24601900dd878b7882e6
Adam <adamcegielka@tlen.pl>
2023-05-25 11:31:03 +0200

    🚀 addition testPaymentByCarsData

---
+++ AutomationExercise-Playwright/test-data/testPaymentByCard.data.ts
@@ -0,0 +1,7 @@
+export const testPaymentByCarsData = {
+    cardVendor: 'Visa',
+    cardNumber: '4070723055941987',
+    cardCvv: '981',
+    cardExpirationateMonth: '11',
+    cardExpirationateYear: '25'
+}
