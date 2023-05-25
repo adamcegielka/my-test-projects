11fdac84ae1f3796344c2f523438ba4409871563
Adam <adamcegielka@tlen.pl>
2023-05-25 13:16:16 +0200

    🔧 code refactoring

--- AutomationExercise-Playwright/tests/testCase16.spec.ts
+++ AutomationExercise-Playwright/tests/testCase16.spec.ts
@@ -1,6 +1,10 @@
 import { test, expect, chromium } from '@playwright/test';
 import { testCase16Data } from '../test-data/testCase16.data';
+import { testRegistrationData } from '../test-data/testRegistration.data';
+import { testPaymentByCarsData } from '../test-data/testPaymentByCard.data';
 import { registerUser } from './testRegistration.spec';
+import { LoginPage } from '../pages/login.page';
+import { CreditCardPage } from '../pages/paymentByCard.page';
 
 test.describe('Test Case 16: Place Order: Login before Checkout', () => {
 
@@ -9,44 +13,50 @@ test.describe('Test Case 16: Place Order: Login before Checkout', () => {
 
   // Test Case 16: Place Order: Login Before Checkout
   test('login before checkout', async ({ page }) => {
-    const url = testCase16Data.url;    
+    // testRegistrationData
+    const userId = testRegistrationData.userId;
+    const userEmail = testRegistrationData.userEmail;
+    const userPassword = testRegistrationData.userPassword;
+    const companyName = testRegistrationData.companyName;
+    const address1 = testRegistrationData.address1;    
+    const country = testRegistrationData.country;
+    const mobileNumber = testRegistrationData.mobileNumber;
+    // testPaymentByCarsData
+    const cardVendor = testPaymentByCarsData.cardVendor;
+    const cardNumber = testPaymentByCarsData.cardNumber;
+    const cardCvv = testPaymentByCarsData.cardCvv;
+    const cardExpirationateMonth = testPaymentByCarsData.cardExpirationateMonth;
+    const cardExpirationateYear = testPaymentByCarsData.cardExpirationateYear;
+    // testCase16Data
     const urlCart = testCase16Data.urlCart;
-    const userId = testCase16Data.userId;
-    const email = testCase16Data.email;
-    const userPassword = testCase16Data.userPassword;
-    const companyName = testCase16Data.companyName;
-    const address1 = testCase16Data.address1;    
-    const country = testCase16Data.country;
-    const mobileNumber = testCase16Data.mobileNumber;
-    const cardVendor = testCase16Data.cardVendor;
-    const cardNumber = testCase16Data.cardNumber;
-    const cardCvv = testCase16Data.cardCvv;
-    const cardExpirationateMonth = testCase16Data.cardExpirationateMonth;
-    const cardExpirationateYear = testCase16Data.cardExpirationateYear;
+    const verifyHomePage = testCase16Data.verifyHomePage;
     const verifyOrder = testCase16Data.verifyOrder;
     const messageText = testCase16Data.messageText;
     const messageOrderConfirmed = testCase16Data.messageOrderConfirmed;
     const verifyShoppingCart = testCase16Data.verifyShoppingCart;
     const verifyAccountDeleted = testCase16Data.verifyAccountDeleted;
-    const verifynameSurname = testCase16Data.verifynameSurname;
+    const verifyNameSurname = testCase16Data.verifyNameSurname;
     const verifyCountryCityZip = testCase16Data.verifyCountryCityZip;
 
     // 1. Launch browser
     await chromium.launch();
 
     // 2. Navigate to url 'http://automationexercise.com'
-    await page.goto(url);
+    await page.goto('/');
 
     // 3. Verify that home page is visible successfully
-    await expect(page).toHaveURL(url);
+    await expect(page).toHaveURL('/');
+    await expect(page).toHaveTitle(verifyHomePage);
 
     // 4. Click 'Signup / Login' button
-    await page.getByRole('link', { name: ' Signup / Login' }).click();
+    await page.getByRole('link', { name: 'Signup / Login' }).click();
 
-    // 5. Fill email, password and click 'Login' button
-    await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill(email);
-    await page.getByPlaceholder('Password').fill(userPassword);
-    await page.getByRole('button', { name: 'Login' }).click();
+    // 5. Fill email, password and click 'Login' button  
+    // POM - Page Object Model
+    const loginPage = new LoginPage(page);
+    await loginPage.userEmail.fill(userEmail);
+    await loginPage.userPassword.fill(userPassword);
+    await loginPage.loginButton.click();
 
     // 6. Verify 'Logged in as username' at top
     await expect(page.getByText(`Logged in as ${userId}`)).toBeVisible();
@@ -66,24 +76,34 @@ test.describe('Test Case 16: Place Order: Login before Checkout', () => {
     await page.getByText('Proceed To Checkout').click();
 
     // 11. Verify Address Details and Review Your Order
-    await expect(page.locator('#address_delivery').getByText(verifynameSurname)).toBeVisible();
+    // testRegistrationData
     await expect(page.locator('#address_delivery').getByText(companyName)).toBeVisible();
     await expect(page.locator('#address_delivery').getByText(address1)).toBeVisible();
-    await expect(page.locator('#address_delivery').getByText(verifyCountryCityZip)).toBeVisible();
     await expect(page.locator('#address_delivery').getByText(country)).toBeVisible();
-    await expect(page.locator('#address_delivery').getByText(mobileNumber)).toBeVisible();    
+    await expect(page.locator('#address_delivery').getByText(mobileNumber)).toBeVisible();
+    // testCase16Data
+    await expect(page.locator('#address_delivery').getByText(verifyNameSurname)).toBeVisible();
+    await expect(page.locator('#address_delivery').getByText(verifyCountryCityZip)).toBeVisible();
     await expect(page.getByRole('row', { name: verifyOrder })).toBeVisible();
 
     // 12. Enter description in comment text area and click 'Place Order'
     await page.locator('textarea[name="message"]').fill(messageText);
     await page.getByRole('link', { name: 'Place Order' }).click();
 
+    // EXIT FROM GOOGLE ADS
+    // await page.frameLocator('iframe[name="aswift_5"]').frameLocator('iframe[name="ad_iframe"]').getByRole('button', { name: 'Close ad' }).click();
+    await page.goto(urlCart);
+    await page.getByText('Proceed To Checkout').click();
+    await page.getByRole('link', { name: 'Place Order' }).click();
+
     // 13. Enter payment details: Name on Card, Card Number, CVC, Expiration date
-    await page.locator('input[name="name_on_card"]').fill(cardVendor);
-    await page.locator('input[name="card_number"]').fill(cardNumber);
-    await page.getByPlaceholder('ex. 311').fill(cardCvv);
-    await page.getByPlaceholder('MM').fill(cardExpirationateMonth);
-    await page.getByPlaceholder('YYYY').fill(cardExpirationateYear);
+    // POM - Page Object Model
+    const creditCardPage = new CreditCardPage(page);
+    await creditCardPage.cardVendor.fill(cardVendor);
+    await creditCardPage.cardNumber.fill(cardNumber);
+    await creditCardPage.cardCvv.fill(cardCvv);
+    await creditCardPage.cardExpirationateMonth.fill(cardExpirationateMonth);
+    await creditCardPage.cardExpirationateYear.fill(cardExpirationateYear);
 
     // 14. Click 'Pay and Confirm Order' button
     await page.getByRole('button', { name: 'Pay and Confirm Order' }).click();
@@ -92,7 +112,7 @@ test.describe('Test Case 16: Place Order: Login before Checkout', () => {
     await expect(page.getByText(messageOrderConfirmed)).toBeVisible();
 
     // 16. Click 'Delete Account' button
-    await page.getByRole('link', { name: ' Delete Account' }).click();
+    await page.getByRole('link', { name: 'Delete Account' }).click();
 
     // 17. Verify 'ACCOUNT DELETED!' and click 'Continue' button
     await expect(page.getByText(verifyAccountDeleted)).toBeVisible();
