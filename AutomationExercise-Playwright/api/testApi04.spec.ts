929f339fac8ab52f168e7524869c636033282a79
Adam <adamcegielka@tlen.pl>
2023-05-26 17:48:32 +0200

    🚀 addition API 4 test

---
+++ AutomationExercise-Playwright/api/testApi04.spec.ts
@@ -0,0 +1,24 @@
+/*
+API 4: PUT To All Brands List
+API URL: https://automationexercise.com/api/brandsList
+Request Method: PUT
+Response Code: 405
+Response Message: This request method is not supported.
+*/
+
+import { test, expect } from '@playwright/test';
+
+test.describe('API 4: PUT To All Brands List', () => {
+  const baseUrl = 'https://automationexercise.com/api';
+
+  test('PUT to all Brands list', async ({ request }) => {
+    // Request Method: PUT
+    const response = await request.put(`${baseUrl}/brandsList`);
+
+    // Response Code: 405
+    // Response Message: This request method is not supported.
+    const responseBody = await response.json();
+    expect(responseBody.message).toBe('This request method is not supported.');
+    console.log(responseBody);
+  });
+});
