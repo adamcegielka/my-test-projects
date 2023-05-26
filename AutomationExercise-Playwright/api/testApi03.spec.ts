070df1726ce61016c14cfee3e493825fc0e9aab1
Adam <adamcegielka@tlen.pl>
2023-05-26 17:35:17 +0200

    🚀 addition API 3 test

---
+++ AutomationExercise-Playwright/api/testApi03.spec.ts
@@ -0,0 +1,25 @@
+/*
+API 3: Get All Brands List
+API URL: https://automationexercise.com/api/brandsList
+Request Method: GET
+Response Code: 200
+Response JSON: All brands list
+*/
+
+import { test, expect } from '@playwright/test';
+
+test.describe.only('API 3: Get All Brands List', () => {
+  const baseUrl = 'https://automationexercise.com/api';
+
+  test('get all brands list', async ({ request }) => {
+    // Request Method: GET
+    const response = await request.get(`${baseUrl}/brandsList`);
+
+    // Verify response code
+    expect(response.status()).toBe(200);
+
+    // Get response JSON
+    const responseBody = JSON.parse(await response.text());
+    console.log(responseBody);
+  });
+});
