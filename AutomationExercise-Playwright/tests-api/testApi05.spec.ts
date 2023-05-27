a95fa765359a84d6b260bf328d4233d44d6770bb
Adam <adamcegielka@tlen.pl>
2023-05-27 16:19:19 +0200

    🔧 update

--- AutomationExercise-Playwright/tests-api/testApi05.spec.ts
+++ AutomationExercise-Playwright/tests-api/testApi05.spec.ts
@@ -13,21 +13,13 @@ test.describe.fixme('API 5: POST To Search Product', () => {
   const baseUrl = 'https://automationexercise.com/api';
 
   test('POST to search product', async ({ request }) => {
-    // Request Method: POST
-    const response = await request.post(`${baseUrl}/searchProduct`,{
-        data: {
-            search_product: 'Men Tshirt',
-        }
-    });
     
-    // Request Parameter: search_product (For example: top, tshirt, jean)
+    const response = await request.post(`${baseUrl}/searchProduct?search_product=tshirt`);
 
-    // Response Code: 200
     expect(response.status()).toBe(200);
 
-    // Response JSON: Searched products list
-    const responseBody = JSON.parse(await response.text());
-    expect(response.ok()).toBeTruthy();
-    console.log(responseBody);
+    const productList = await response.json();
+    expect(productList).toContain('tshirt');
+    console.log(productList);
   });
 });
