da82e0c5d649e117e0bdaed3f102e4d2d4a5d92b
Adam <adamcegielka@tlen.pl>
2023-05-27 10:11:07 +0200

    🔧 renaming the api test folder

--- AutomationExercise-Playwright/api.config.ts
+++ AutomationExercise-Playwright/api.config.ts
@@ -3,7 +3,7 @@ import { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
     timeout: 60000,
     retries: 0,
-    testDir: 'api',
+    testDir: 'tests-api',
     use: {
         headless: true,
         viewport: { width: 1280, height: 720 },
