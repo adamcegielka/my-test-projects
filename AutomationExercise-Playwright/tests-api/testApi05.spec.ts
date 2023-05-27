24f2c74097718be19ba9a94d1344c9a97fe2aa00
Adam <adamcegielka@tlen.pl>
2023-05-27 15:28:57 +0200

    🔧 update

--- AutomationExercise-Playwright/tests-api/testApi05.spec.ts
+++ AutomationExercise-Playwright/tests-api/testApi05.spec.ts
@@ -27,10 +27,6 @@ test.describe.fixme('API 5: POST To Search Product', () => {
 
     // Response JSON: Searched products list
     const responseBody = JSON.parse(await response.text());
-    // expect(responseBody.id).toBe(2);
-    // expect(responseBody.name).toBe('Men Tshirt');
-    // expect(responseBody.brand).toBe('H&M');
-    // expect(responseBody.price).toBeTruthy();
     expect(response.ok()).toBeTruthy();
     console.log(responseBody);
   });
