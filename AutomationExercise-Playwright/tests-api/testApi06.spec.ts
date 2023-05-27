c0779396f3b4fd2115d179197f7c97054392ee4e
Adam <adamcegielka@tlen.pl>
2023-05-27 10:19:53 +0200

    🚀 adding the file to the API 6 tests

--- AutomationExercise-Playwright/tests-api/testApi06.spec.ts
+++ AutomationExercise-Playwright/tests-api/testApi06.spec.ts
@@ -5,3 +5,21 @@ Request Method: POST
 Response Code: 400
 Response Message: Bad request, search_product parameter is missing in POST request.
 */
+
+import { test, expect } from '@playwright/test';
+
+test.describe('API 6: POST To Search Product without search_product parameter', () => {
+  const baseUrl = 'https://automationexercise.com/api';
+
+  test('POST to search product without search_product parameter', async ({ request }) => {
+    // Request Method: POST
+    const response = await request.post(`${baseUrl}/searchProduct`,{
+        // ...
+    });
+
+    // Response Code: 400
+    expect(response.status()).toBe(400);
+
+    // Response Message: Bad request, search_product parameter is missing in POST request.
+  });
+});
