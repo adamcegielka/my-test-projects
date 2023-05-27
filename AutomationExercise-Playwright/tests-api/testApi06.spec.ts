831b990089a1d6c482fc404828aacae77dfcb249
Adam <adamcegielka@tlen.pl>
2023-05-27 17:18:46 +0200

    🔧 update

--- AutomationExercise-Playwright/tests-api/testApi06.spec.ts
+++ AutomationExercise-Playwright/tests-api/testApi06.spec.ts
@@ -8,18 +8,18 @@ Response Message: Bad request, search_product parameter is missing in POST reque
 
 import { test, expect } from '@playwright/test';
 
-test.describe('API 6: POST To Search Product without search_product parameter', () => {
+test.describe.fixme('API 6: POST To Search Product without search_product parameter', () => {
   const baseUrl = 'https://automationexercise.com/api';
 
   test('POST to search product without search_product parameter', async ({ request }) => {
-    // Request Method: POST
-    const response = await request.post(`${baseUrl}/searchProduct`,{
-        // ...
+    
+    const response = await request.post(`${baseUrl}/searchProduct`, {
+      data: {
+        'search_product': ''
+      }
     });
-
-    // Response Code: 400
-    expect(response.status()).toBe(400);
-
-    // Response Message: Bad request, search_product parameter is missing in POST request.
+        
+    expect(response.status()).toBe(400); 
+    console.log(await response.json());
   });
 });
