502a36083d68a5daaec4cc721e1e7c0367d413bd
Adam <adamcegielka@tlen.pl>
2023-05-28 12:34:53 +0200

    🔧 update Test API 11

--- AutomationExercise-Playwright/tests-api/testApi11.spec.ts
+++ AutomationExercise-Playwright/tests-api/testApi11.spec.ts
@@ -13,35 +13,30 @@ test.describe.fixme('API 11: POST To Create/Register User Account', () => {
   const baseUrl = 'https://automationexercise.com/api';
 
   test('POST to Create/Register user account', async ({ request }) => {
-    const newUser = {
-      'name': 'John Doe',
-      'email': 'johndoe@example.com',
-      'password': 'secretpassword',
-      'title': 'Mr',
-      'birth_date': '06',
-      'birth_month': '08',
-      'birth_year': '1990',
-      'firstname': 'John',
-      'lastname': 'Doe',
-      'company': 'Example Company',
-      'address1': '123 Main Street',
-      'address2': 'Apt 4B',
-      'country': 'United States',
-      'zipcode': '12345',
-      'state': 'California',
-      'city': 'Los Angeles',
-      'mobile_number': '1234567890'
-    };
+    const response = await request.post(`${baseUrl}/createAccount`, {
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: 'secretpassword',
+        title: 'Mr',
+        birth_date: '06',
+        birth_month: '08',
+        birth_year: '1990',
+        firstname: 'John',
+        lastname: 'Doe',
+        company: 'Example Company',
+        address1: '123 Main Street',
+        address2: 'Apt 4B',
+        country: 'United States',
+        zipcode: '12345',
+        state: 'California',
+        city: 'Los Angeles',
+        mobile_number: '1234567890',
+      },
+    });
 
-    // const response = await request.post(`${baseUrl}/createAccount`, { json: newUser });
-
-    // expect(response.status()).toBe(201);
-
-    // const responseBody = await response.json();
-    // console.log(responseBody);
-
-    // expect(responseBody.name).toBe(newUser.name);
-    // expect(responseBody.createAccount).toBeTruthy();
-    // expect(responseBody.message).toBe('User created!');
+    expect(response.status()).toBe(201);
+    expect(await response.json()).toBe('User created!');
+    console.log(await response.json());
   });
 });
