1595990ec66e43050824fbc3bbbc98bdf4cce158
Adam <adamcegielka@tlen.pl>
2023-06-03 17:01:14 +0200

    🚀 adding DeletionUser class

---
+++ AutomationExercise-Playwright/page-objects/DeletionUser.ts
@@ -0,0 +1,21 @@
+import { Locator, Page } from '@playwright/test';
+
+export class DeletionUser {
+  readonly page: Page;
+  readonly deleteButton: Locator;
+  readonly continueButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.deleteButton = page.getByRole('link', { name: 'Delete Account' });
+    this.continueButton = page.getByRole('link', { name: 'Continue' });
+  }
+
+  async clickDeleteButton() {
+    await this.deleteButton.click();
+  }
+
+  async clickContinueButton() {
+    await this.continueButton.click();
+  }
+}
