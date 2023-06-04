d0dfeb599801c35a3d0e88615921e8dfe21aeed8
Adam <adamcegielka@tlen.pl>
2023-06-04 08:59:20 +0200

    🔧 adding loginIncorrect

--- AutomationExercise-Playwright/page-objects/LoginPage.ts
+++ AutomationExercise-Playwright/page-objects/LoginPage.ts
@@ -3,32 +3,48 @@ import { testRegistrationData } from '../test-data/testRegistration.data';
 
 export class LoginPage {
   readonly page: Page;
-  readonly userEmailInpot: Locator;
-  readonly userPasswordInpot: Locator;
+  readonly userEmailInput: Locator;
+  readonly userPasswordInput: Locator;
   readonly clickButton: Locator;
   readonly logoutButton: Locator;
+  readonly userEmailInputIncorrect: Locator;
+  readonly userPasswordInputIncorrect: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.userEmailInpot = page
+    this.userEmailInput = page
       .locator('form')
       .filter({ hasText: 'Login' })
       .getByPlaceholder('Email Address');
-    this.userPasswordInpot = page.getByPlaceholder('Password');
+    this.userPasswordInput = page.getByPlaceholder('Password');
     this.clickButton = page.getByRole('button', { name: 'Login' });
     this.logoutButton = page.getByRole('link', { name: 'Logout' });
+    this.userEmailInputIncorrect = page
+    .locator('form')
+    .filter({ hasText: 'Login' })
+    .getByPlaceholder('Email Address');
+    this.userPasswordInputIncorrect = page.getByPlaceholder('Password');
   }
 
   async login() {
     const userEmail = testRegistrationData.userEmail;
     const userPassword = testRegistrationData.userPassword;
 
-    await this.userEmailInpot.fill(userEmail);
-    await this.userPasswordInpot.fill(userPassword);
+    await this.userEmailInput.fill(userEmail);
+    await this.userPasswordInput.fill(userPassword);
     await this.clickButton.click();
   }
 
   async logout() {
     await this.logoutButton.click();
+  }
+
+  async loginIncorrect() {
+    const userEmailIncorrect = testRegistrationData.userEmailIncorrect;
+    const userPasswordIncorrect = testRegistrationData.userPasswordIncorrect;
+
+    await this.userEmailInputIncorrect.fill(userEmailIncorrect);
+    await this.userPasswordInputIncorrect.fill(userPasswordIncorrect);
+    await this.clickButton.click();
   }
 }
