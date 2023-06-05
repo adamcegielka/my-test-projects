086c4027a0ff03573b0c4e4151d7ae9fef63da3a
Adam <adamcegielka@tlen.pl>
2023-06-05 16:16:41 +0200

    🔧 update

--- AutomationExercise-Playwright/tests-api/testApi02.spec.ts
+++ AutomationExercise-Playwright/tests-api/testApi02.spec.ts
@@ -11,11 +11,10 @@ import { test, expect } from '@playwright/test';
 test.describe('API 2: POST To All Products List', () => {
   const baseUrl = 'https://automationexercise.com/api';
 
-  test('POST to all products list', async ({ request }) => {
-    
+  test('POST to all products list', async ({ request }) => {    
     const response = await request.post(`${baseUrl}/productsList`);
-
     const responseBody = await response.json();
+    
     expect(responseBody.message).toBe('This request method is not supported.');
     console.log(responseBody);
   });
