3b7b1e86e44495ecd4bf29ac384727a02563f2b9
Adam <adamcegielka@tlen.pl>
2023-06-05 15:52:38 +0200

    🔧 update

--- AutomationExercise-Playwright/tests-api/testApi01.spec.ts
+++ AutomationExercise-Playwright/tests-api/testApi01.spec.ts
@@ -12,14 +12,10 @@ test.describe('API 1: Get All Products List', () => {
   const baseUrl = 'https://automationexercise.com/api';
 
   test('get all products list', async ({ request }) => {
-    // Request Method: GET
     const response = await request.get(`${baseUrl}/productsList`);
-
-    // Verify response code
-    expect(response.status()).toBe(200);
-
-    // Get response JSON
     const responseBody = JSON.parse(await response.text());
+
+    expect(response.status()).toBe(200);
     console.log(responseBody);
   });
 
