cd68124379e21564835a0e3e8989d4ebbedba047
Adam <adamcegielka@tlen.pl>
2023-06-06 12:41:16 +0200

    🔧 correction of error

--- AutomationExercise-Playwright/tests/testCase06.spec.ts
+++ AutomationExercise-Playwright/tests/testCase06.spec.ts
@@ -3,7 +3,7 @@ import { testCase06Data } from '../test-data/testCase06.data';
 
 test.describe('Test Case 6: Contact Us Form', () => {
   
-  test('contact us form', async ({ page }) => {    
+  test.only('contact us form', async ({ page }) => {    
     const userId = testCase06Data.userId;
     const userEmail = testCase06Data.userEmail;
     const subject = testCase06Data.subject;
@@ -49,13 +49,17 @@ test.describe('Test Case 6: Contact Us Form', () => {
     await expect(page.locator('.status.alert.alert-success').getByText(verifySuccessMessage)).toBeVisible({ timeout: 5000 });
 
     // 11. Click 'Home' button and verify that landed to home page successfully
-    await page.click('.fa.fa-angle-double-left');
+    // await page.getByRole('link', { name: 'Home' }).click();
+    await page.click('#form-section > .btn.btn-success');
     
     // EXIT FROM GOOGLE ADS
-    // await page.frameLocator('iframe[name="aswift_2"]').frameLocator('iframe[name="ad_iframe"]').getByRole('button', { name: 'Close ad' }).click();    
+    // await page.frameLocator('iframe[name="aswift_2"]').frameLocator('iframe[name="ad_iframe"]').getByRole('button', { name: 'Close ad' }).click();
+    await page.goBack();
+    await page.goForward();
+    
     await expect(page).toHaveURL('/');
     await expect(page).toHaveTitle(verifyHomePage);
-
+    
     // CLOSE BROWSER
     await page.close();
   });  
