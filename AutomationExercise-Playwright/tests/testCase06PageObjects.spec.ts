8ea5b4bf1c44e1040d60c2603cf83e0736699e05
Adam <adamcegielka@tlen.pl>
2023-06-06 14:23:46 +0200

    🔧 addition of description

--- AutomationExercise-Playwright/tests/testCase06PageObjects.spec.ts
+++ AutomationExercise-Playwright/tests/testCase06PageObjects.spec.ts
@@ -37,3 +37,17 @@ test.describe('Test Case 6: Contact Us Form', () => {
     await page.close();
   });  
 });
+
+/*
+1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Verify that home page is visible successfully
+4. Click on 'Contact Us' button
+5. Verify 'GET IN TOUCH' is visible
+6. Enter name, email, subject and message
+7. Upload file
+8. Click 'Submit' button
+9. Click OK button
+10. Verify success message 'Success! Your details have been submitted successfully.' is visible
+11. Click 'Home' button and verify that landed to home page successfully
+*/
