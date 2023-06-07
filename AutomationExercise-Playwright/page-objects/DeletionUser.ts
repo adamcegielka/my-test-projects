7c4c115feeec09239a24cd3068b1d2f03bb29d73
Adam <adamcegielka@tlen.pl>
2023-06-07 16:37:47 +0200

    🔧 update

--- AutomationExercise-Playwright/page-objects/DeletionUser.ts
+++ AutomationExercise-Playwright/page-objects/DeletionUser.ts
@@ -1,14 +1,16 @@
-import { Locator, Page } from '@playwright/test';
+import { Locator, Page, expect } from '@playwright/test';
 
 export class DeletionUser {
   readonly page: Page;
   readonly deleteButton: Locator;
   readonly continueButton: Locator;
+  readonly verifyMessage: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.deleteButton = page.getByRole('link', { name: 'Delete Account' });
     this.continueButton = page.getByRole('link', { name: 'Continue' });
+    this.verifyMessage = page.locator('.title.text-center');
   }
 
   async clickDeleteButton() {
@@ -22,5 +24,9 @@ export class DeletionUser {
   async deletionNewUser() {    
     await this.deleteButton.click();
     await this.continueButton.click();
+  }
+
+  async messageAccountDeleted() {
+    await expect(this.verifyMessage).toContainText('Account Deleted!');
   }
 }
