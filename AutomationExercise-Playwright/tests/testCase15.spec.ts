384f8cd3dd3f98cf5bc851953294f673f01bffc8
Adam <adamcegielka@tlen.pl>
2023-06-10 13:08:01 +0200

    🔧 refactoring

--- AutomationExercise-Playwright/tests/testCase15.spec.ts
+++ AutomationExercise-Playwright/tests/testCase15.spec.ts
@@ -113,9 +113,8 @@ test.describe('Test Case 15: Place Order: Register before Checkout', () => {
 
     // EXIT FROM GOOGLE ADS
     // await page.frameLocator('iframe[name="aswift_5"]').frameLocator('iframe[name="ad_iframe"]').getByRole('button', { name: 'Close ad' }).click();
-    await page.goto(urlCart);
-    await page.getByText('Proceed To Checkout').click();
-    await page.getByRole('link', { name: 'Place Order' }).click();
+    await page.goBack();
+    await page.goForward();
 
     // 14. Enter payment details: Name on Card, Card Number, CVC, Expiration date   
     // POM - Page Object Model
