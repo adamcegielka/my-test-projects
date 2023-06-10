e4a4d53df0a9144eafde0961b0ca3a5bb818d732
Adam <adamcegielka@tlen.pl>
2023-06-10 12:30:44 +0200

    🔧 update

--- AutomationExercise-Playwright/test-data/testCase16.data.ts
+++ AutomationExercise-Playwright/test-data/testCase16.data.ts
@@ -2,7 +2,7 @@ export const testCase16Data = {
     verifyHomePage: 'Automation Exercise',
     urlCart: 'https://automationexercise.com/view_cart',
     verifyNameSurname: 'Mr. Adrian Jackson',
-    verifyCountryCityZip: 'Washington Washington 84739',
+    verifyCountryCityZip: 'Seattle Washington 84739',
     verifyOrder: 'Product Image Winter Top Women > Tops Rs. 600 1 Rs. 600',
     messageText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     messageOrderConfirmed: 'Congratulations! Your order has been confirmed!',
