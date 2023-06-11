2f7b20c2025256257a96ec0e13908316fb33a008
Adam <adamcegielka@tlen.pl>
2023-06-11 16:46:44 +0200

    🔧 update

--- AutomationExercise-Playwright/page-objects/CartPage.ts
+++ AutomationExercise-Playwright/page-objects/CartPage.ts
@@ -40,6 +40,30 @@ export class CartPage {
     await expect(this.addressDelivery).toContainText(mobileNumber);
   }
 
+  async assertAddressBillingy() {
+    const userTitle = testRegistrationData.userTitle;
+    const firstName = testRegistrationData.firstName;
+    const lastName = testRegistrationData.lastName;
+    const companyName = testRegistrationData.companyName;
+    const address1 = testRegistrationData.address1;
+    const city = testRegistrationData.city;
+    const state = testRegistrationData.state;
+    const zipCode = testRegistrationData.zipCode;
+    const country = testRegistrationData.country;
+    const mobileNumber = testRegistrationData.mobileNumber;
+
+    await expect(this.addressDelivery).toContainText(userTitle);
+    await expect(this.addressDelivery).toContainText(firstName);
+    await expect(this.addressDelivery).toContainText(lastName);
+    await expect(this.addressDelivery).toContainText(companyName);
+    await expect(this.addressDelivery).toContainText(address1);
+    await expect(this.addressDelivery).toContainText(city);
+    await expect(this.addressDelivery).toContainText(state);
+    await expect(this.addressDelivery).toContainText(zipCode);
+    await expect(this.addressDelivery).toContainText(country);
+    await expect(this.addressDelivery).toContainText(mobileNumber);
+  }
+
   async clickbuttonProToCheckout() {
     await this.buttonProceedToCheckout.click();
   }
