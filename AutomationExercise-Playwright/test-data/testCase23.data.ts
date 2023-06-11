2f392bead479f54d441a04cb2ea939fdc7ccf589
Adam <adamcegielka@tlen.pl>
2023-06-11 15:25:23 +0200

    🔧 update

--- AutomationExercise-Playwright/test-data/testCase23.data.ts
+++ AutomationExercise-Playwright/test-data/testCase23.data.ts
@@ -2,10 +2,10 @@ export const testCase23Data = {
     verifyHomePage: 'Automation Exercise',
     urlCart: 'https://automationexercise.com/view_cart',
     verifyNameSurname: 'Mr. Adrian Jackson',
-    verifyCountryCityZip: 'Washington Washington 84739',
+    verifyCountryCityZip: 'Seattle Washington 84739',
     verifyShoppingCart: 'Shopping Cart',
     verifyAccountCreated: 'ACCOUNT CREATED!',
     verifyAccountDeleted: 'ACCOUNT DELETED!',
-    verifyDeliveryAddress: 'Your delivery address Mr. Adrian Jackson Porta Limited 50 Herzi Street APT 93 Wa',
-    verifyBillingyAddress: 'Your billing address Mr. Adrian Jackson Porta Limited 50 Herzi Street APT 93 Was'
+    verifyDeliveryAddress: 'Your delivery address Mr. Adrian Jackson Porta Limited 50 Herzi Street APT 93 Seattle Washington 84739 United States 801222333',
+    verifyBillingyAddress: 'Your billing address Mr. Adrian Jackson Porta Limited 50 Herzi Street APT 93 Seattle Washington 84739 United States 801222333'
 }
