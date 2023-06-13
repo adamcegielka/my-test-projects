4166fbe7ea4f278875d8638c150816321d2e0bdc
Adam <adamcegielka@tlen.pl>
2023-06-13 14:48:22 +0200

    🚀 adding testCase25PageObjects

---
+++ AutomationExercise-Playwright/tests/testCase25PageObjects.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect, chromium } from '@playwright/test';
+import { testCase25Data } from '../test-data/testCase25.data';
+import { HomePage } from '../page-objects/HomePage';
+
+test.describe('Test Case 25: Verify Scroll Up using "Arrow" button and Scroll Down functionality', () => {
+
+  test('verify scroll up using "Arrow" button and scroll down functionality', async ({ page }) => {
+    const homePage = new HomePage(page);
+
+    const verifySubscription = testCase25Data.verifySubscription;
+    const verifyVisibleScreen = testCase25Data.verifyVisibleScreen;
+
+    await chromium.launch();
+    await homePage.navHomePage();
+    await homePage.verifyHomePage();
+    const scrollFooter = page.locator('footer');
+    await scrollFooter.scrollIntoViewIfNeeded();
+    await expect(page.getByText(verifySubscription)).toBeVisible();
+    await page.locator('.grippy-host').click(); // HIDING ADVERTISING
+    await page.locator('#scrollUp').click();   
+    await expect(page.locator('.logo.pull-left')).toBeInViewport();
+    await expect(page.getByRole('heading', { name: verifyVisibleScreen })).toBeInViewport();
+  });
+});
+
+/*
+1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Verify that home page is visible successfully
+4. Scroll down page to bottom
+5. Verify 'SUBSCRIPTION' is visible
+6. Click on arrow at bottom right side to move upward
+7. Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen
+*/
