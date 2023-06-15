08d06a9f0927af4006397ba0936c6785c656c3b5
Adam <adamcegielka@tlen.pl>
2023-06-15 11:11:00 +0200

    🔧 refactoring

--- AutomationExercise-Playwright/tests-api/testApi01.spec.ts
+++ AutomationExercise-Playwright/tests-api/testApi01.spec.ts
@@ -11,12 +11,11 @@ import { test, expect } from '@playwright/test';
 test.describe('API 1: Get All Products List', () => {
   const baseUrl = 'https://automationexercise.com/api';
 
-  test('get all products list', async ({ request }) => {
+  test.only('get all products list', async ({ request }) => {
     const response = await request.get(`${baseUrl}/productsList`);
-    const responseBody = JSON.parse(await response.text());
-
-    expect(response.status()).toBe(200);
-    console.log(responseBody);
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);   
+    console.log(await response.json());
   });
 
 
@@ -25,7 +24,8 @@ test.describe('API 1: Get All Products List', () => {
   test.skip('another way - get all products list', async ({ request }) => {
     const response = await request.get(`${baseUrl}/productsList`);
 
-    expect(response.status()).toBe(200);    
-    console.log(await response.json());
+    expect(response.status()).toBe(200); 
+    const responseBody = JSON.parse(await response.text());
+    console.log(responseBody);
   });
 });
